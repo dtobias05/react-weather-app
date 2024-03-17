cd3e58d3ac5ef2e0945c5f19f56c718416def4d1
dtobias05 <dtobias05@outlook.com>
2024-03-16 22:27:03 -0500

    Added Github repo URL

--- src/App.js
+++ src/App.js
@@ -2,5 +2,20 @@ import React from "react";
 import "./App.css";
 
 function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <footer>
+        This project was coded by Denise Tobias and is {""}
+        <a
+          href="https://github.com/dtobias05/react-weather-app"
+          target="_blank"
+        >
+          open-sourced on Github
+        </a>
+      </footer>
+    </div>
+  );
 }
+
+export default App;
