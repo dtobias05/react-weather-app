71e8d2a0ebe84524b118e03062e5182dd6602bfd
dtobias05 <dtobias05@outlook.com>
2024-07-28 14:29:21 -0500

    Added new components javascript and a CSS for forecast

--- src/Weatherinfo.js
+++ src/Weatherinfo.js
@@ -19,6 +19,7 @@ export default function WeatherInfo(props) {
             <div className="Float-left">
               <WeatherIcon
                 code={props.data.icon}
+                size={52}
                 alt={props.data.description}
               />
             </div>
