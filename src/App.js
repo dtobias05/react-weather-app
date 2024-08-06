7713e630614ad08e40674d230b997de6123c8305
dtobias05 <dtobias05@outlook.com>
2024-08-05 22:04:10 -0500

    Corrected warning

--- src/App.js
+++ src/App.js
@@ -8,7 +8,9 @@ function App() {
       <div className="container">
         <Weather defaultCity="Chicago" />
         <footer>
-          This project was coded by Denise Tobias and is {""}
+          This project was coded by{" "}
+          <a href="https://gilded-cat-2aaba1.netlify.app">Denise Tobias</a> and
+          is {""}
           <a
             href="https://github.com/dtobias05/react-weather-app"
             target="_blank"
